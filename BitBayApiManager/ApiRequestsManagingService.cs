56b2a5c6b846f6548ef8aac095790f1423b4c3b0
Marcin Szyszka <marcin.szyszka@action.pl>
2018-01-29 14:06:46 +0100

    Added project with requests managing service

---
+++ BitBayApiManager/ApiRequestsManagingService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitBayApiManager
+{
+    public class ApiRequestsManagingService : IApiRequestsManagingService
+    {
+        private readonly ConcurrentDictionary<long, object> _waitingRequests;
+
+        public ApiRequestsManagingService()
+        {
+            _waitingRequests = new ConcurrentDictionary<long, object>();
+        }
+
+        public async Task<TOut> MakeRequest<TOut, TIn>(Func<TIn, Task<TOut>> requestAction, TIn parameter)
+        {
+            _waitingRequests.TryAdd(DateTime.Now.Ticks, requestAction);
+
+            var result = default(TOut);
+
+            var executed = false;
+
+            while (!executed)
+            {
+                await Task.Delay(1000);
+
+                var firstRequestInQeue = _waitingRequests.OrderByDescending(r => r.Key).First();
+
+                if ((firstRequestInQeue.Value as Func<TIn, Task<TOut>>) == requestAction)
+                {
+                    result = await requestAction(parameter);
+
+                    var deleted = false;
+
+                    while (!deleted)
+                    {
+                        if (DeleteReuqest(firstRequestInQeue))
+                        {
+                            deleted = true;
+                        }
+                    }
+
+                    executed = true;
+                }
+            }
+
+            return result;
+        }
+
+        private bool DeleteReuqest(KeyValuePair<long, object> firstRequestInQeue)
+        {
+            return _waitingRequests.TryRemove(firstRequestInQeue.Key, out var requestToRemove);
+        }
+    }
+}
---
+++ BitBayApiManager/IApiRequestsManagingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BitBayApiManager
+{
+    public interface IApiRequestsManagingService
+    {
+        Task<TOut> MakeRequest<TOut, TIn>(Func<TIn, Task<TOut>> requestAction, TIn parameter);
+    }
+}
